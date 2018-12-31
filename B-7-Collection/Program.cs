90d14e3b31edc9349675d8b86519423d3bcfb4d7
Дима Ярута <dima.yaruta@gmail.com>
2018-12-31 14:15:29 +0300

    Task 7

---
+++ B-7-Collection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayListExample();
+            
+            Console.ReadLine();
+        }
+
+        public static void ArrayListExample()
+        {
+            var list = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Nest row please");
+                list.Add(Console.ReadLine());
+            }
+
+            foreach (var row in list)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+       
+        public class Song
+        {
+            
+        }
+                          
+    }
+}
